ed07c12ee0a534080ef9550d839f0135db76c36c
Thomas Spriggs <thomas.e.spriggs@gmail.com>
2020-04-29 11:16:58 +0100

    fixed check columns

--- tests/test_check_columns.py
+++ tests/test_check_columns.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 from pl_curve import check_columns
 import pandas
-import numpy as np
 
 
 def test_check_columns_correct():
     '''FIXME: Implement this test
     Tests check_columns correctly checks items summing to 1.0 returns true
     '''
-    a = np.array([0.5],[0.5])
-    assert check_columns(a) == True
+    df = pandas.DataFrame([0.5,0.5])
+    assert check_columns(df) is True
 
 
 def test_check_columns_incorrect():
